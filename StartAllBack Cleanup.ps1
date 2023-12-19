68fb5ed63ad822da2f2b61cf978d1f18e1ea0123
dazzaXx <48131734+dazzaXx@users.noreply.github.com>
2023-12-19 12:51:28 +0000

    Update StartAllBack Cleanup.ps1
    Trimmed.

--- StartAllBack Cleanup.ps1
+++ StartAllBack Cleanup.ps1
@@ -1,17 +1,9 @@
-﻿Write-Output  ''
-Write-Output  'StartAllBack CleanUp by iandiv'
-Write-Output  'This script will clean up the registry keys'
-Write-Output  ''
-Write-Output  'CAUTION!! Use this scripts at your own risk. '
-Write-Output  'I am not responsible for any damage that may occur as a result of running this script.'
-Write-Output  ''
-Write-Output 'Do you want to begin?'
-$x = Read-Host '[Y/N]'
-if (-Not($x -eq "Y" -or ($x -eq "y"))) {
-    Write-Output  'cancelled by user!'
-    exit
-}
-Write-Output  ''
+#Trimmed down version of "https://github.com/iandiv/StartAllBack-Cleanup", for automation with Task Scheduler.
+
+#Wait for explorer to load what it needs to before executing. 
+#Prevents script from killing explorer too early leading to it not restarting properly.
+
+sleep -s 30
 
 $keys = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\* | Select-Object -ExpandProperty Name
 $keys = $keys | Where-Object { $_ -cmatch '\{[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{8}.*$' }
@@ -25,24 +17,8 @@ foreach ($key in $keys) {
     if (-Not ($subkeys_count -eq 0)) {
         continue
     }
-
-    Write-Output  '' 
-    Write-Output  ('Cleaning..:')
-    Write-Output  '' 
+ 
     Remove-Item -Path ('Registry::' + $key)
-    
-    
 }
 
-Write-Output  ('Cleaned Successfully!')
-Write-Output  '' 
-Write-Output  'Restarting explorer...'
 stop-process -name explorer –force
-Write-Output  '' 
-Write-Output  'All DONE' 
-Write-Output  '' 
-Write-Host "Press any key to Exit..."
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.SendKeys]::SendWait('%{F4}')
